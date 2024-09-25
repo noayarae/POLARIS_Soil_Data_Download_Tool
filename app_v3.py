034221f8950af798c1e76e4a3c014fe91dbfd3ab
noayarae <enoay7@yahoo.com>
2024-09-25 18:26:01 -0500

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -1,3 +1,13 @@
+"""
+Created on Wed Jun 24 17:46:07 2024
+@author: efrain.noa-yarasca
+
+This code launches the "POLARIS Soil Data Download" tool, enabling users to download customized POLARIS soil data 
+for a specified region. The primary input required is the destination folder for storing the downloaded raster files. 
+Users then select the desired soil parameters and define the geographic bounds of the area by specifying the 
+Left, Right, Top, and Bottom coordinates. Once these inputs are configured, clicking the download button 
+initiates the retrieval of the selected POLARIS data.
+"""
 
 import os, requests
 import tkinter as tk
